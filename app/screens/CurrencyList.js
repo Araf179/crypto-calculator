a6fee7d3f944ead7a23417f691b3003386b15d9e
Arafullah <shihabaraf@gmail.com>
2017-12-28 15:28:31 -0500

    Added Search Filter

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { FlatList, StatusBar, View } from 'react-native';
+import { FlatList, StatusBar, View, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies';
-
+import SearchInput, { createFilter } from 'react-native-search-filter';
 import { ListItem, Separator } from '../components/list';
 import currencies from '../data/currencies';
 import crypto from '../data/crypto';
@@ -11,6 +11,14 @@ import crypto from '../data/crypto';
 const TEMP_CURRENT_CURRENCY = 'CAD';
 
 class CurrencyList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      cryptoFilter: crypto,
+      currencyFilter: currencies
+    }
+  }
   static propTypes = {
     navigation: PropTypes.object,
     dispatch: PropTypes.func,
@@ -29,6 +37,17 @@ class CurrencyList extends Component {
     this.props.navigation.goBack(null);
   };
 
+  handleSearch = (term) => {
+    this.setState({ searchTerm: term });
+    const { type } = this.props.navigation.state.params;
+    if (type === 'base') {
+      this.setState({cryptoFilter: crypto.filter(createFilter(this.state.searchTerm, null)) });
+    } else if (type === 'quote') {
+      this.setState({currencyFilter: currencies.filter(createFilter(this.state.searchTerm, null)) });
+    }
+    console.log(this.state.cryptoFilter);
+  }
+
   render() {
     let comparisonCurrency = this.props.baseCurrency;
     if (this.props.navigation.state.params.type === 'quote') {
@@ -38,8 +57,13 @@ class CurrencyList extends Component {
     return (
       <View style={{ flex: 1 }}>
         <StatusBar translucent={false} barStyle="default" />
+        <SearchInput 
+          onChangeText={(term) => { this.handleSearch(term) }} 
+          style={styles.searchInput}
+          placeholder="Type a message to search"
+          />
         <FlatList
-          data={this.props.navigation.state.params.type === 'base' ? crypto : currencies}
+          data={this.props.navigation.state.params.type === 'base' ? this.state.cryptoFilter : this.state.currencyFilter}
           renderItem={({ item }) => (
             <ListItem
               text={item}
@@ -55,6 +79,15 @@ class CurrencyList extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  searchInput:{
+    padding: 10,
+    borderColor: '#CCC',
+    borderWidth: 1
+  }
+});
+
 
 const mapStateToProps = state => ({
   baseCurrency: state.currencies.baseCurrency,
