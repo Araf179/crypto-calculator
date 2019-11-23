cd7f60bf87f02f30405ee76fc443aa97c473efa9
Arafullah Shihab <shihabaraf@gmail.com>
2019-11-23 12:39:51 -0500

    Updated expo to version 35.0.0

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, createStackNavigator } from 'react-navigation';
 
 import Home from '../screens/Home';
 import CurrencyList from '../screens/CurrencyList';
